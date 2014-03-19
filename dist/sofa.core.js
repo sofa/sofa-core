6036ddf9af54a34451ef38f5b1eb8c5401ec2325
Pascal Precht <pascal.precht@googlemail.com>
2014-03-19 17:33:48 +0100

    chore(*): releases 0.5.2

--- dist/sofa.core.js
+++ dist/sofa.core.js
@@ -1,3 +1,11 @@
+/**
+ * sofa-core - v0.5.1 - 2014-03-19
+ * http://www.sofa.io
+ *
+ * Copyright (c) 2013 CouchCommerce GmbH (http://www.couchcommerce.org) and other contributors
+ * THIS SOFTWARE CONTAINS COMPONENTS OF THE SOFA SDK (SOFA.IO).
+ * IT IS PROVIDED UNDER THE LICENSE TERMS OF THE ATTACHED LICENSE.TXT.
+ */
 ;(function (window, undefined) {
 
 'use strict';
